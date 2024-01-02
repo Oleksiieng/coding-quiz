c0e4e9945fd9bb6ed0505de9fd6e094ca0462b15
Oleksii Kulykov <oleksii.eng@gmail.com>
2024-01-02 18:39:04 +0000

    added questions

---
+++ assets/js/questions.js
@@ -0,0 +1,32 @@
+const questions = [
+    {
+        question: "What does CSS stand for?",
+        choices: ["Central Style Sheets", "Cascading Style Sheets", "Colorful Style Sheets", "Computer Style Sheets"],
+        answer: "Cascading Style Sheets"
+    },
+    {
+        question: "Which language runs in a web browser?",
+        choices: ["Java", "C", "Python", "JavaScript"],
+        answer: "JavaScript"
+    },
+    {
+        question: "What does HTML stand for?",
+        choices: ["Hyper Trainer Marking Language", "Hyper Text Marketing Language", "Hyper Text Markup Language", "Hyper Text Markup Leveler"],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        question: "In JavaScript, what will `console.log(typeof NaN)` output?",
+        choices: ["'number'", "'NaN'", "'undefined'", "'error'"],
+        answer: "'number'"
+    },
+    {
+        question: "Which JavaScript method is used to find an element by its ID?",
+        choices: ["getElementById()", "getElementByName()", "querySelector()", "findElementById()"],
+        answer: "getElementById()"
+    },
+    {
+        question: "Which of the following is a self-closing tag in HTML?",
+        choices: ["<img>", "<div>", "<p>", "<h1>"],
+        answer: "<img>"
+    }
+];
